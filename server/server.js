04716e3cec14419379be779f4a12aea59bc3c68e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-11-05 11:30:29 -0700

    login API fixed

--- server/server.js
+++ server/server.js
@@ -2,43 +2,39 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const {
-    login,
-    getUserByEmail,
-    createUser,
-    deleteUserByEmail,
-    getBuildingsByEmail,
-    getBuildingByName,
-    createBuilding,
-    updateBuildingByName,
-    deleteBuildingByName,
-    getWorkspaces,
-    getWorkspaceByName,
-    getWorkspacesByBuildingNameAndWorkSpaceName,
-    getWorkspacesByBuildingName,
-    createWorkspace,
-    updateWorkspaceByName,
-    deleteWorkspaceByName
+  login,
+  getUserByEmail,
+  createUser,
+  deleteUserByEmail,
+  getBuildingsByEmail,
+  getBuildingByName,
+  createBuilding,
+  updateBuildingByName,
+  deleteBuildingByName,
+  getWorkspaces,
+  getWorkspaceByName,
+  getWorkspacesByBuildingNameAndWorkSpaceName,
+  getWorkspacesByBuildingName,
+  createWorkspace,
+  updateWorkspaceByName,
+  deleteWorkspaceByName,
 } = require("./database");
 
-
 app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await login(email, password);
-    if (user == null ){
-        res.status(400).send("Wrong email or password");
-    }
+  const { email, password } = req.body;
+
+  const user = await login(email, password);
+  if (user === null) {
+    res.status(400).send("Wrong email or password");
+  } else {
     res.json(user);
+  }
 });
 
-
-
-
-
 app.listen(5544, () => {
-    console.log("Server is listening on port 5544");
+  console.log("Server is listening on port 5544");
 });
