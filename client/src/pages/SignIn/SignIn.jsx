8ed5e0008e13861b7a7468051edd829e029c20be
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-11-13 13:13:14 -0700

    Sign in can differentiate between owner or ...

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom"; // Import useForm hook
+import { useForm } from "react-hook-form"; // Import useForm hook
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -12,29 +11,13 @@ import "./SignIn.css";
 
 const SignIn = () => {
   const { register, handleSubmit, reset } = useForm(); // Initialize useForm
-  const navigate = useNavigate();
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post("http://localhost:5544/login", data);
+      const response = await axios.post("http://localhost:5544/login", data); // Change this to POST
 
-      //console.log("Response from the server:", response.data);
-
-      if (response.status === 200) {
-        //console.log("Login successful. User data:", response.data);
-
-        // Check if the user is an owner based on the server response
-        if (response.data[0].owner === 1 ) {
-          // User is an owner, handle accordingly (redirect or other actions)
-          console.log("User is an owner");
-        } else {
-          // User is not an owner, redirect to CoworkersPage
-          console.log("User is not an owner. Redirecting to CoworkersPage.");
-          navigate("/coworkerspage");
-        }
-
-        reset(); // Reset the form
-      }
+      console.log("Response from the server:", response.data);
+      reset(); // Reset the form
     } catch (error) {
       console.error("Error while submitting the form:", error);
     }
